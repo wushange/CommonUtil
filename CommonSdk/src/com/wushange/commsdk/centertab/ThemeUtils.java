0f178c5998e2c27d4289c566f2b9eb4b44a6a464
wushge11 <309646776@qq.com>
2016-05-31 14:52:46 +0800

    支持中间凸起TAB

---
+++ CommonSdk/src/com/wushange/commsdk/centertab/ThemeUtils.java
@@ -0,0 +1,23 @@
+package com.wushange.commsdk.centertab;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+
+import com.wushange.commsdk.R;
+
+public class ThemeUtils {
+
+    private static final int[] APPCOMPAT_CHECK_ATTRS = {R.attr.colorPrimary};
+
+    public static void checkAppCompatTheme(Context context) {
+        TypedArray a = context.obtainStyledAttributes(APPCOMPAT_CHECK_ATTRS);
+        final boolean failed = !a.hasValue(0);
+        if (a != null) {
+            a.recycle();
+        }
+        if (failed) {
+            throw new IllegalArgumentException("You need to use a Theme.AppCompat theme "
+                    + "(or descendant) with the design library.");
+        }
+    }
+}
